1ece9683a67458321a6dd738785b4c1c89885e41
Alvaro Passos <apassos@e-goi.com>
2018-09-14 00:57:21 +0100

    incidents dashboard beta version

--- frontend/src/api/api-config.js
+++ frontend/src/api/api-config.js
@@ -13,13 +13,13 @@
   __API_SOCKET_PORT__
 */
 const BACKEND_HOST = window.location.hostname;
-const API_PORT = __API_PORT__ || 13030;
+const API_PORT = __API_PORT__ || 3030;
 const SOCKET_PORT = __API_SOCKET_PORT__ || 9092;
 const STORE_PORT = __API_STORE_PORT__ || 6060;
 
-const API_URL = `http://${__API_URL__ || BACKEND_HOST}`;
-const SOCKET_URL = `ws://${BACKEND_HOST || 'localhost'}`;
-const STORE_URL = `http://${__API_STORE_HOST__ || 'localhost'}`;
+const API_URL = `http://${'192.168.1.3' || BACKEND_HOST}`;
+const SOCKET_URL = `ws://${BACKEND_HOST || '192.168.1.3'}`;
+const STORE_URL = `http://${__API_STORE_HOST__ || '192.168.1.3'}`;
 
 export const AUTH_ADDRESS = `${API_URL}:${API_PORT}`;
 export const API_ADDRESS = `${API_URL}:${API_PORT}`;
--- frontend/src/components/dashboard/dashboard.module.js
+++ frontend/src/components/dashboard/dashboard.module.js
@@ -3,7 +3,6 @@ import { DashboardComponent, DashboardState } from './dashboard.component';
 import { DashboardService } from './dashboard.service';
 import { DashViewerModule } from './dash-viewer/dash-viewer.module';
 
-
 export const DashboardModule = angular.module('dashboard', ['ui.router', DashViewerModule])
   .component('dashboardView', DashboardComponent)
   .service('DashboardService', DashboardService)
