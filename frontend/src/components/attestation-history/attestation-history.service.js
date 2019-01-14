5806211cf3db37a239ea93a9a4ad348d94a0b748
Luís Conceição <lconceicao@ubiwhere.com>
2019-01-14 11:20:42 +0000

    Frontend: added missing files of TM integration

---
+++ frontend/src/components/attestation-history/attestation-history.service.js
@@ -0,0 +1,48 @@
+import { API_ADDRESS } from 'api/api-config';
+
+const API_VNSF_NOTIFICATIONS = `${API_ADDRESS}/tm/vnsf/notifications`;
+const API_TM_NOTIFICATIONS = `${API_ADDRESS}/tm/notifications`;
+
+export class AttestationHistoryService {
+  constructor($http, $q, toastr, AuthService, TenantsService) {
+    'ngInject';
+
+    this.q = $q;
+    this.http = $http;
+    this.toast = toastr;
+    this.authService = AuthService;
+    this.tenantsService = TenantsService;
+  }
+
+  getNotifications({ page = 1, limit = 25 }, filters = {}) {
+    const params = { page, nocache: (new Date()).getTime(), sort: '[("time",-1)]' };
+    if (Object.keys(filters).length) params.where = JSON.stringify(filters);
+    let api_address;
+    if (this.authService.isUserTenantAdmin()) {
+      api_address = API_VNSF_NOTIFICATIONS;
+    } else {
+      api_address = API_TM_NOTIFICATIONS;
+    }
+    return this.http.get(api_address, { params })
+      .then((response) => {
+        const notifs = response.data._items;
+
+        return {
+          notifs,
+          meta: response.data._meta,
+        };
+      });
+  }
+
+  getRemediation(id, data, etag) {
+    let api_address;
+    if (this.authService.isUserTenantAdmin()) {
+      api_address = API_VNSF_NOTIFICATIONS;
+    } else {
+      api_address = API_TM_NOTIFICATIONS;
+    }
+    return this.http.patch(`${api_address}/${id}`, data, { headers: { 'If-Match': etag } });
+  }
+}
+
+export default AttestationHistoryService;
---
+++ frontend/src/components/inventory/inventory.component.js
@@ -0,0 +1,49 @@
+import template from './inventory.html';
+
+const UI_STRINGS = {
+  title: 'NS inventory',
+  tableTitle: 'Inventory',
+};
+
+const TABLE_HEADERS = {
+  custom_tags: 'Tags',
+  ref_id: 'Id',
+  _created: 'Created',
+};
+
+export const InventoryComponent = {
+  template,
+  controller: class InventoryComponent {
+    constructor(InventoryService) {
+      'ngInject';
+
+      this.strings = UI_STRINGS;
+      this.inventoryService = InventoryService;
+      this.createOpen = false;
+      this.deleteOpen = false;
+
+      this.offset = 0;
+      this.limit = 25;
+      this.filters = {};
+      this.headers = { ...TABLE_HEADERS };
+    }
+
+    $onInit() {
+      this.inventoryService.getInventoryServices({
+        page: this.offset,
+        limit: this.limit,
+      }, this.filters)
+        .then((items) => {
+          this.items = items;
+        });
+    }
+  },
+};
+
+export const inventoryState = {
+  parent: 'home',
+  name: 'nsinventory',
+  url: '/inventory',
+  component: 'inventoryView',
+};
+
